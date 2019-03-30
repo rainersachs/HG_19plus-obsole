493f615caf2840f890d8e354cac73e8cfac1b099
Edward Huang <edwardghg@gmail.com>
2019-03-29 19:08:05 -0700

    Trivial typo fix.

--- data_info.R
+++ data_info.R
@@ -53,7 +53,7 @@ rm(list=ls())
 # include gamma-ray data. Includes LET=100 keV/micron for Ti, an ad-hoc compromise
 # between lower value at beam entry and higher value at mouse cage.
 
-ion_data <- data.frame(read.csv("one-ion.csv")) 
+ion_data <- data.frame(read.csv("one_ion.csv")) 
 mix_data <- data.frame(read.csv("mix_ion.csv")) 
 # The two .csv files contain all input HG data. Changes in the data set such as additions, corrections, or deletions should be made only in the .csv files. Such changes will then be implemented automatically by the scripts everywhere else.
 
